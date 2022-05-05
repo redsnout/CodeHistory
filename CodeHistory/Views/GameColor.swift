590e77f29a9175988f18a2fb81ea6947354b6f0e
srome <srome11@gmail.com>
2022-05-05 17:26:38 -0500

    Changed colors on answered questions

--- CodeHistory/Views/GameColor.swift
+++ CodeHistory/Views/GameColor.swift
@@ -13,7 +13,7 @@ struct GameColor {
     
     static let main = Color(red: 20/255, green: 28/255, blue: 58/255)
     static let accent = Color(red: 48/255, green: 105/255, blue: 240/255)
-    static let correctGuess = Color.green
-    static let incorrectGuess = Color.red
+    static let correctGuess = Color.blue
+    static let incorrectGuess = Color.orange
     
 }
